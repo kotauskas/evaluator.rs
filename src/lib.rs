1c73cddf1503124d930f2a8a972329a0ffcc599a
Kotauskas <v.toncharov@gmail.com>
2020-01-04 18:47:47 +0300

    Added dummy evaluators

--- src/lib.rs
+++ src/lib.rs
@@ -134,3 +134,35 @@ where Out: Clone, Cl: FnMut() -> Out {
         Out::clone(&self.last.clone().unwrap())
     }
 }
+
+/// Implements dummy evaluation of clonable **and** non-clonable types: every time the `eval()` method is called, a fixed value is returned.
+pub struct DummyEval<Out> {
+    value: Out
+}
+impl<Out> From<Out> for DummyEval<Out> {
+    /// Constructs the evaluator from the specified dummy value.
+    fn from(value: Out) -> Self {
+        DummyEval {value}
+    }
+}
+impl<Out> Evaluator for DummyEval<Out>
+where Out: Clone {
+    type Output = Out;
+    /// Evaluates and returns the result. **This includes the overhead of cloning the value, because the fixed value must be preserved.
+    ///
+    /// Because caching evaluators, as their name suggests, store the cached value inside themselves, a mutable borrow of `self` is required.
+    /// # Panics
+    /// May panic only if cloning of the inner value panics.
+    fn eval(&mut self) -> Self::Output {
+        self.value.clone()
+    }
+}/*
+impl<Out> RcEvaluator for DummyEval<Out> {
+    type Output = Out;
+    /// Evaluates and returns an [`std::rc::Rc`] to the result. Because evaluators may possibly involve caching, a mutable borrow of `self` is required by the [`Evaluator`] trait, though dummy evaluators obviously do not involve any, as no values are produced during evaluation.
+    /// # Panics
+    /// Guaranteed to never panic.
+    fn eval(&mut self) -> Rc<Self::Output> {
+        uuh dunno?
+    }
+}*/
