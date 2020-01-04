12d8cf8eac762c9be682cc673ae8b86dd2ea61c3
Kotauskas <v.toncharov@gmail.com>
2020-01-04 18:36:07 +0300

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,38 @@
+//! Provides interfaces and implementations for logic constructs that return values without accepting arguments, either via closure captures, global state, calculation or otherwise.
+
 #[cfg(test)]
 mod tests;
 
 use std::rc::Rc;
 
-/// Trait for describing objects that can be used to evaluate something.
+/// An interface for immediate evaluation (without caching) for any types and cached (lazy) evaluation for types that implement [`Clone`], including those that implement [`Copy`], integers and floats being the simplest examples.
 pub trait Evaluator {
-    /// 
+    /// The type that the evaluator evaluates to.
     type Output;
+    /// Evaluates and returns the result. Because evaluators may possibly involve caching, a mutable borrow of `self` is required. Thus all evaluators must be mutable.
+    /// # Panics
+    /// Panicking of this function is not defined or restricted. Refer to the documentation of implementing structs for more information.
     fn eval(&mut self) -> Self::Output;
 }
 
+/// An interface for cached (lazy) evaluation for types that do not implement [`Clone`] (note that `Clone` is a supertrait of [`Copy`]).
 pub trait RcEvaluator {
+    /// The type that the evaluator evaluates to.
     type Output;
+    /// Evaluates and returns a reference counter to the result. Because evaluators may possibly involve caching, a mutable borrow of `self` is required. Thus all evaluators must be mutable.
+    /// # Panics
+    /// Panicking of this function is not defined or restricted. Refer to the documentation of implementing structs for more information.
     fn eval(&mut self) -> Rc<Self::Output>;
 }
 
+/// Implements immediate evaluation via closures.
 pub struct ImmEval<Out, Cl>
 where Cl: FnMut() -> Out {
     closure: Cl
 }
 impl<Out, Cl> From<Cl> for ImmEval<Out, Cl>
 where Cl: FnMut() -> Out {
+    /// Constructs the evaluator from the specified closure.
     fn from(cl: Cl) -> Self {
         ImmEval {closure: cl}
     }
@@ -28,12 +40,15 @@ where Cl: FnMut() -> Out {
 impl<Out, Cl> Evaluator for ImmEval<Out, Cl>
 where Cl: FnMut() -> Out {
     type Output = Out;
-    
+    /// Evaluates and returns the result. Because evaluators may possibly involve caching, a mutable borrow of `self` is required by the [`Evaluator`] trait, though immediate evaluators do not involve any.
+    /// # Panics
+    /// Panicking of this function is not defined or restricted and depends on the closure.
     fn eval(&mut self) -> Self::Output {
         (self.closure)()
     }
 }
 
+/// Implements cached (lazy) evaluation **of clonable types** via closures.
 pub struct Eval<Out, Cl>
 where Out: Clone, Cl: FnMut() -> Out {
     closure: Cl,
@@ -41,12 +56,14 @@ where Out: Clone, Cl: FnMut() -> Out {
 }
 impl<Out, Cl> Eval<Out, Cl>
 where Out: Clone, Cl: FnMut() -> Out {
+    /// Invalidates the cached value, if there is one. If the evaluator was never used after creation or was recently `flush()`ed, does nothing.
     pub fn flush(&mut self) {
         self.last = None;
     }
 }
 impl<Out, Cl> From<Cl> for Eval<Out, Cl>
 where Out: Clone, Cl: FnMut() -> Out {
+    /// Constructs the evaluator from the specified closure.
     fn from(cl: Cl) -> Self {
         Eval {closure: cl, last: None}
     }
@@ -54,7 +71,11 @@ where Out: Clone, Cl: FnMut() -> Out {
 impl<Out, Cl> Evaluator for Eval<Out, Cl>
 where Out: Clone, Cl: FnMut() -> Out {
     type Output = Out;
-    
+    /// Evaluates and returns the result. **This includes the overhead of cloning the value, because the internal cache must be preserved. If you need to move the result every time it is evaluated, use [`ImmEval`].**
+    ///
+    /// Because caching evaluators, as their name suggests, store the cached value inside themselves, a mutable borrow of `self` is required.
+    /// # Panics
+    /// Panicking of this function is not defined or restricted and depends on the closure.
     fn eval(&mut self) -> Self::Output {
         if self.last.is_none() {
             self.last = Option::from((self.closure)());
@@ -63,6 +84,7 @@ where Out: Clone, Cl: FnMut() -> Out {
     }
 }
 
+/// Implements cached (lazy) evaluation of clonable **and** non-clonable types via closures.
 pub struct RcEval<Out, Cl>
 where Cl: FnMut() -> Out {
     closure: Cl,
@@ -70,12 +92,14 @@ where Cl: FnMut() -> Out {
 }
 impl<Out, Cl> RcEval<Out, Cl>
 where Cl: FnMut() -> Out {
+    /// Disowns the cached value (destroying it if there are no other owners), if there is one. If the evaluator was never used after creation or was recently `flush()`ed, does nothing.
     pub fn flush(&mut self) {
         self.last = None;
     }
 }
 impl<Out, Cl> From<Cl> for RcEval<Out, Cl>
 where Cl: FnMut() -> Out {
+    /// Constructs the evaluator from the specified closure.
     fn from(cl: Cl) -> Self {
         RcEval {closure: cl, last: None}
     }
@@ -83,7 +107,11 @@ where Cl: FnMut() -> Out {
 impl<Out, Cl> RcEvaluator for RcEval<Out, Cl>
 where Cl: FnMut() -> Out {
     type Output = Out;
-    
+    /// Evaluates and returns an [`std::rc::Rc`] to the result.
+    ///
+    /// Because caching evaluators, as their name suggests, store the cached value inside themselves, a mutable borrow of `self` is required.
+    /// # Panics
+    /// Panicking of this function is not defined or restricted and depends on the closure.
     fn eval(&mut self) -> Rc<Self::Output> {
         if self.last.is_none() {
             self.last = Option::from(Rc::from((self.closure)()));
@@ -94,7 +122,11 @@ where Cl: FnMut() -> Out {
 impl<Out, Cl> Evaluator for RcEval<Out, Cl>
 where Out: Clone, Cl: FnMut() -> Out {
     type Output = Out;
-    
+    /// Evaluates and returns the result. **This includes the overhead of cloning the value, because the internal cache must be preserved. If you need to move the result every time it is evaluated, use [`ImmEval`].**
+    ///
+    /// Because caching evaluators, as their name suggests, store the cached value inside themselves, a mutable borrow of `self` is required.
+    /// # Panics
+    /// Panicking of this function is not defined or restricted and depends on the closure.
     fn eval(&mut self) -> Self::Output {
         if self.last.is_none() {
             self.last = Option::from(Rc::from((self.closure)()));
